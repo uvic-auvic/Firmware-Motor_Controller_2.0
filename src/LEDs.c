9d2fe944174a4e603b3c91f221256cda6e1ff6c5
Poorna Chander Kalidas <poornakalidas@gmail.com>
2018-03-28 18:38:06 -0700

    Changed status LED setup to match schematic. Pins PA4 and PA5.

--- src/LEDs.c
+++ src/LEDs.c
@@ -10,21 +10,16 @@
 extern void init_LED(){
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	//Andy's attempt at blinking an LED
-
-	//Enable the D port to be used
-	//RCC->AHB1ENR |= RCC_AHB1Periph_GPIOD;
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	//Enable the A port
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	//For now use the GPIO_Init function, to save time
-	//Be careful because not erasing any of the past settings so failure to overwrite will keep them
+	//Be careful because not erasing any of the past settings
 
-	//Select port 3 to be our toggle port
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	//Configure pin PA4(red LED) and pin PA5(blue LED) as output
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_SET);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
