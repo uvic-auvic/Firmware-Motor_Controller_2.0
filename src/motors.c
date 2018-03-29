946b65fb5fc029009a0d203c975ea13778ee7962
Poorna Chander Kalidas <poornakalidas@gmail.com>
2018-03-28 19:01:16 -0700

    Made corrections to comments.

--- src/motors.c
+++ src/motors.c
@@ -25,7 +25,7 @@ uint8_t motorDirection = 0b00000000;
 /* Initialization functions
  * ----------------------------------------------------------
  */
-static void init_motor_current_temp_MUX(){
+static void init_motor_current_temp_MUX() {
 	//Enable clock
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
@@ -41,7 +41,8 @@ static void init_motor_current_temp_MUX(){
 
 	//GPIOD Configuration
 	GPIO_InitTypeDef GPIOD_InitStruct;
-	GPIOD_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;;
+	GPIOD_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
+	;
 	GPIOD_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
 	GPIOD_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIOD_InitStruct.GPIO_OType = GPIO_OType_PP;
@@ -49,15 +50,15 @@ static void init_motor_current_temp_MUX(){
 	GPIO_Init(GPIOD, &GPIOD_InitStruct);
 }
 
-static void init_motor_speed_control()
-{
+static void init_motor_speed_control() {
 
 }
 
 static void init_PWM_in_timer() {
 
 	// Enable peripheral clock for TIM1, TIM3, TIM4, TIM5
-	RCC->APB1ENR |= RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4 | RCC_APB1Periph_TIM5;
+	RCC->APB1ENR |= RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4
+			| RCC_APB1Periph_TIM5;
 	RCC->APB2ENR |= RCC_APB2Periph_TIM1;
 
 	/* Time base configuration */
@@ -117,7 +118,9 @@ static void init_PWM_in_timer() {
 static void init_PWM_in_GPIO() {
 
 	// GPIOA clock enable
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_AHB1PeriphClockCmd(
+			RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOB,
+			ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -201,57 +204,63 @@ static void init_PWM_in_DMA() {
 
 	// DMA Configuration: Motor 1, PA1, TIM5_CH2, DMA1 Stream4 Channel6
 	DMA_InitStructure.DMA_Channel = DMA_Channel_6;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor1 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor1 - 1];
 	DMA_Init(DMA1_Stream4, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream4, ENABLE); //Enable the DMA1 - Stream 4
 
 	// DMA Configuration: Motor 2, PA2, TIM5_CH3, DMA1 Stream0 Channel6
 	DMA_InitStructure.DMA_Channel = DMA_Channel_6;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor2 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor2 - 1];
 	DMA_Init(DMA1_Stream0, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream0, ENABLE); //Enable the DMA1 - Stream 0
 
 	// DMA Configuration: Motor 3, PA11, TIM1_CH4, DMA2 Stream4 Channel6
 	DMA_InitStructure.DMA_Channel = DMA_Channel_6;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor3 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor3 - 1];
 	DMA_Init(DMA2_Stream4, &DMA_InitStructure);
 	DMA_Cmd(DMA2_Stream4, ENABLE); //Enable the DMA2 - Stream 4
 
 	// DMA Configuration: Motor 4, PA9, TIM1_CH2, DMA2 Stream2 Channel6
 	DMA_InitStructure.DMA_Channel = DMA_Channel_6;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor4 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor4 - 1];
 	DMA_Init(DMA2_Stream2, &DMA_InitStructure);
 	DMA_Cmd(DMA2_Stream2, ENABLE); //Enable the DMA2 - Stream 2
 
 	// DMA Configuration: Motor 5, PD14, TIM4_CH3, DMA1 Stream7 Channel2
 	DMA_InitStructure.DMA_Channel = DMA_Channel_2;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor5 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor5 - 1];
 	DMA_Init(DMA1_Stream7, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream7, ENABLE); //Enable the DMA1 - Stream 7
 
 	// DMA Configuration: Motor 6, PD13, TIM4_CH2, DMA1 Stream3 Channel2
 	DMA_InitStructure.DMA_Channel = DMA_Channel_2;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor6 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor6 - 1];
 	DMA_Init(DMA1_Stream3, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream3, ENABLE); //Enable the DMA1 - Stream 3
 
 	// DMA Configuration: Motor 7, PA7, TIM3_CH2, DMA1 Stream5 Channel5
 	DMA_InitStructure.DMA_Channel = DMA_Channel_5;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor7 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor7 - 1];
 	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream5, ENABLE); //Enable the DMA1 - Stream 5
 
 	// DMA Configuration: Motor 8, PB1, TIM3_CH4, DMA1 Stream2 Channel5
 	DMA_InitStructure.DMA_Channel = DMA_Channel_5;
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) pwmInTimestamp[Motor8 - 1];
+	DMA_InitStructure.DMA_Memory0BaseAddr =
+			(uint32_t) pwmInTimestamp[Motor8 - 1];
 	DMA_Init(DMA1_Stream2, &DMA_InitStructure);
 	DMA_Cmd(DMA1_Stream2, ENABLE); //Enable the DMA1 - Stream 2
 
 }
 
-
-static void init_motor_speed_feedback()
-{
+static void init_motor_speed_feedback() {
 
 	init_PWM_in_timer();
 	init_PWM_in_GPIO();
@@ -270,8 +279,7 @@ static void enable_timers() {
 }
 
 /* Extern declarations in each section must go below static declarations */
-extern void init_motors()
-{
+extern void init_motors() {
 	/* Initialize sub-modules */
 	init_motor_current_temp_MUX();
 	init_motor_speed_control();
@@ -287,8 +295,8 @@ extern void init_motors()
 /* Put any static/hidden functions your code may need below here: */
 
 /* Robert's extern/visible functions go below here: */
-extern void motor_set_speed_percent(motors_t motor_x, uint8_t speed, direction_t dir)
-{
+extern void motor_set_speed_percent(motors_t motor_x, uint8_t speed,
+		direction_t dir) {
 
 }
 
@@ -297,19 +305,15 @@ extern void motor_set_speed_percent(motors_t motor_x, uint8_t speed, direction_t
  * ----------------------------------------------------------
  */
 
-/* Put any static/hidden functions your code may need below here: */
-
-
 /* Poorna's extern/visible functions go below here: */
 
 /*
  * Returns motor RPM
  * Param: Motor number
  * Returns: Motor RPM, signed 16-bit int, positive value = forward rotation, negative value = reverse rotation
- *          Max RPM value = 32767. If actual rpm exceeds this, function will return 32767.
+ *          Max RPM value = 32767. If actual rpm exceeds this, this value will overflow.
  */
-extern int16_t motor_get_rpm(motors_t motor_x)
-{
+extern int16_t motor_get_rpm(motors_t motor_x) {
 	/*
 	 * Below is the motor to Timer mapping for RPM
 	 * Motor 1 - TIM5 CH2
@@ -321,23 +325,18 @@ extern int16_t motor_get_rpm(motors_t motor_x)
 	 * Motor 7 - TIM3 CH2
 	 * Motor 8 - TIM3 CH4
 	 */
-	static uint16_t *motorToTIM_DIER[NUMBER_OF_MOTORS] = {	&TIM5->DIER,
-															&TIM5->DIER,
-															&TIM1->DIER,
-															&TIM1->DIER,
-															&TIM4->DIER,
-															&TIM4->DIER,
-															&TIM3->DIER,
-															&TIM3->DIER		};
+	static uint16_t *motorToTIM_DIER[NUMBER_OF_MOTORS] = { &TIM5->DIER,
+			&TIM5->DIER, &TIM1->DIER, &TIM1->DIER, &TIM4->DIER, &TIM4->DIER,
+			&TIM3->DIER, &TIM3->DIER };
 
-	static uint16_t motorToTIM_CCxDE[NUMBER_OF_MOTORS] = { 	TIM_IT_CC2DE,
-															TIM_IT_CC3DE,
-															TIM_IT_CC4DE,
-															TIM_IT_CC2DE,
-															TIM_IT_CC3DE,
-															TIM_IT_CC2DE,
-															TIM_IT_CC2DE,
-															TIM_IT_CC4DE	};
+	static uint16_t motorToTIM_CCxDE[NUMBER_OF_MOTORS] = { TIM_IT_CC2DE,
+	TIM_IT_CC3DE,
+	TIM_IT_CC4DE,
+	TIM_IT_CC2DE,
+	TIM_IT_CC3DE,
+	TIM_IT_CC2DE,
+	TIM_IT_CC2DE,
+	TIM_IT_CC4DE };
 
 	/*
 	 * Below is the motor to DMA mapping for RPM
@@ -350,32 +349,31 @@ extern int16_t motor_get_rpm(motors_t motor_x)
 	 * Motor 7 - DMA1 Stream5 CH5
 	 * Motor 8 - DMA1 Stream2 CH5
 	 */
-	static uint32_t *motorToDMA_NTDR[NUMBER_OF_MOTORS] = {	&DMA1_Stream4->NDTR,
-															&DMA1_Stream0->NDTR,
-															&DMA2_Stream4->NDTR,
-															&DMA2_Stream2->NDTR,
-															&DMA1_Stream7->NDTR,
-															&DMA1_Stream3->NDTR,
-															&DMA1_Stream5->NDTR,
-															&DMA1_Stream2->NDTR		};
-
+	static uint32_t *motorToDMA_NTDR[NUMBER_OF_MOTORS] = { &DMA1_Stream4->NDTR,
+			&DMA1_Stream0->NDTR, &DMA2_Stream4->NDTR, &DMA2_Stream2->NDTR,
+			&DMA1_Stream7->NDTR, &DMA1_Stream3->NDTR, &DMA1_Stream5->NDTR,
+			&DMA1_Stream2->NDTR };
 
 	//need to stop raw data from updating
 	*motorToTIM_DIER[motor_x - 1] &= ~motorToTIM_CCxDE[motor_x - 1];
 
-	uint8_t idx = PWM_IN_ARRAY_LENGTH - (*motorToDMA_NTDR[motor_x - 1]); //Index in array that was most recently updated
-	uint8_t stop_idx = (idx + PWM_IN_ARRAY_LENGTH -1) % PWM_IN_ARRAY_LENGTH; //Index that is most out of date
+	uint8_t idx = PWM_IN_ARRAY_LENGTH - (*motorToDMA_NTDR[motor_x - 1]); //Index of array element that was most recently updated
+	uint8_t stop_idx = (idx + PWM_IN_ARRAY_LENGTH - 1) % PWM_IN_ARRAY_LENGTH; //Index of array element that is most out of date
 	float frequency = 0;
 	int16_t rpm = 0;
 
 	for (; idx != stop_idx; idx = ((idx + 1) % PWM_IN_ARRAY_LENGTH)) {
 		uint32_t diff = 0; // Temporarily store the difference between two timestamps
 
-		if (pwmInTimestamp[motor_x - 1][(idx + 1) % PWM_IN_ARRAY_LENGTH] > pwmInTimestamp[motor_x - 1][idx]) {
-			diff = pwmInTimestamp[motor_x - 1][(idx + 1) % PWM_IN_ARRAY_LENGTH] - pwmInTimestamp[motor_x - 1][idx];
+		if (pwmInTimestamp[motor_x - 1][(idx + 1) % PWM_IN_ARRAY_LENGTH]
+				> pwmInTimestamp[motor_x - 1][idx]) {
+			diff = pwmInTimestamp[motor_x - 1][(idx + 1) % PWM_IN_ARRAY_LENGTH]
+					- pwmInTimestamp[motor_x - 1][idx];
 
 		} else { //if not bigger, must be smaller
-			diff = (_100_MHZ - pwmInTimestamp[motor_x - 1][idx]) + pwmInTimestamp[motor_x - 1][(idx + 1) % PWM_IN_ARRAY_LENGTH];
+			diff = (_100_MHZ - pwmInTimestamp[motor_x - 1][idx])
+					+ pwmInTimestamp[motor_x - 1][(idx + 1)
+							% PWM_IN_ARRAY_LENGTH];
 		}
 		frequency += diff; // Add the differences. Will be averaged later
 	}
@@ -388,13 +386,12 @@ extern int16_t motor_get_rpm(motors_t motor_x)
 	rpm = frequency * FREQ_TO_RPM_CONV * INTERNAL_OSC_CALIB; //Convert frequency to rpm and apply correction for internal oscillator
 	// Not checking for overflow. Motor RPM will likely never reach the 32767rpm.
 
-	if((motorDirection >> (motor_x - 1)) & 0x1) { // If bit (motor_x - 2) is 0, motor is spinning forward, reverse otherwise
+	if ((motorDirection >> (motor_x - 1)) & 0x1) { // If bit (motor_x - 2) is 0, motor is spinning forward, reverse otherwise
 		rpm *= -1;
 	}
 
 	return rpm;
 }
-
 
 /* Code from Issue #8 (by Robert Keen).
  * Reading MUX'ed sensor data.
@@ -403,80 +400,80 @@ extern int16_t motor_get_rpm(motors_t motor_x)
 
 extern uint8_t get_motor_current(motor_sensors_t motor_sensor_x) {
 	switch (motor_sensor_x) {
-	case Motor_Curr_ADC1:
-		GPIOC->BSRRL &= ~(GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9);//turn off PC7-PC9
-		return 0;
-		break;
-	case Motor_Curr_ADC2:
-		GPIOC->BSRRL |= GPIO_Pin_7;	//turn on PC7
-		return 0;
-		break;
-	case Motor_Curr_ADC3:
-		GPIOC->BSRRL |= GPIO_Pin_8; //turn on PC8
-		return 0;
-		break;
-	case Motor_Curr_ADC4:
-		GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8; //turn on PC7-PC8
-		return 0;
-		break;
-	case Motor_Curr_ADC5:
-		GPIOC->BSRRL |= GPIO_Pin_9; //turn on PC9
-		return 0;
-		break;
-	case Motor_Curr_ADC6:
-		GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_9; //turn on PC7-PC9
-		return 0;
-		break;
-	case Motor_Curr_ADC7:
-		GPIOC->BSRRL |= GPIO_Pin_8 | GPIO_Pin_9; //turn on PC8-PC9
-		return 0;
-		break;
-	case Motor_Curr_ADC8:
-		GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9; //turn on PC7-PC9
-		return 0;
-		break;
-	default:
-		return 0;
-		break;
+		case Motor_Curr_ADC1:
+			GPIOC->BSRRL &= ~(GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9); //turn off PC7-PC9
+			return 0;
+			break;
+		case Motor_Curr_ADC2:
+			GPIOC->BSRRL |= GPIO_Pin_7;	//turn on PC7
+			return 0;
+			break;
+		case Motor_Curr_ADC3:
+			GPIOC->BSRRL |= GPIO_Pin_8; //turn on PC8
+			return 0;
+			break;
+		case Motor_Curr_ADC4:
+			GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8; //turn on PC7-PC8
+			return 0;
+			break;
+		case Motor_Curr_ADC5:
+			GPIOC->BSRRL |= GPIO_Pin_9; //turn on PC9
+			return 0;
+			break;
+		case Motor_Curr_ADC6:
+			GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_9; //turn on PC7-PC9
+			return 0;
+			break;
+		case Motor_Curr_ADC7:
+			GPIOC->BSRRL |= GPIO_Pin_8 | GPIO_Pin_9; //turn on PC8-PC9
+			return 0;
+			break;
+		case Motor_Curr_ADC8:
+			GPIOC->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9; //turn on PC7-PC9
+			return 0;
+			break;
+		default:
+			return 0;
+			break;
 	}
 }
 
 extern uint8_t get_motor_temp(motor_sensors_t motor_sensor_x) {
 	switch (motor_sensor_x) {
-	case Motor_Temp_ADC1:
-		GPIOD->BSRRL &= ~(GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9); //turn off PD7-PD9
-		return 0;
-		break;
-	case Motor_Temp_ADC2:
-		GPIOD->BSRRL |= GPIO_Pin_7; //turn on PD7
-		return 0;
-		break;
-	case Motor_Temp_ADC3:
-		GPIOD->BSRRL |= GPIO_Pin_8; //turn on PD8
-		return 0;
-		break;
-	case Motor_Temp_ADC4:
-		GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8; //turn on PD7-PD8
-		return 0;
-		break;
-	case Motor_Temp_ADC5:
-		GPIOD->BSRRL |= GPIO_Pin_9; //turn on PD9
-		return 0;
-		break;
-	case Motor_Temp_ADC6:
-		GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_9; //turn on PD7-PD9
-		return 0;
-		break;
-	case Motor_Temp_ADC7:
-		GPIOD->BSRRL |= GPIO_Pin_8 | GPIO_Pin_9; //turn on PD8-PD9
-		return 0;
-		break;
-	case Motor_Temp_ADC8:
-		GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9; //turn on PD7-PD9
-		return 0;
-		break;
-	default:
-		return 0;
-		break;
+		case Motor_Temp_ADC1:
+			GPIOD->BSRRL &= ~(GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9); //turn off PD7-PD9
+			return 0;
+			break;
+		case Motor_Temp_ADC2:
+			GPIOD->BSRRL |= GPIO_Pin_7; //turn on PD7
+			return 0;
+			break;
+		case Motor_Temp_ADC3:
+			GPIOD->BSRRL |= GPIO_Pin_8; //turn on PD8
+			return 0;
+			break;
+		case Motor_Temp_ADC4:
+			GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8; //turn on PD7-PD8
+			return 0;
+			break;
+		case Motor_Temp_ADC5:
+			GPIOD->BSRRL |= GPIO_Pin_9; //turn on PD9
+			return 0;
+			break;
+		case Motor_Temp_ADC6:
+			GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_9; //turn on PD7-PD9
+			return 0;
+			break;
+		case Motor_Temp_ADC7:
+			GPIOD->BSRRL |= GPIO_Pin_8 | GPIO_Pin_9; //turn on PD8-PD9
+			return 0;
+			break;
+		case Motor_Temp_ADC8:
+			GPIOD->BSRRL |= GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9; //turn on PD7-PD9
+			return 0;
+			break;
+		default:
+			return 0;
+			break;
 	}
 }
