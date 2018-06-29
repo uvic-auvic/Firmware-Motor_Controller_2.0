abd0e446483603687586f182426a988e508ddfa1
Poorna Chander Kalidas <poornakalidas@gmail.com>
2018-06-29 01:47:30 -0700

    Added debug_write

---
+++ inc/debug.h
@@ -0,0 +1,13 @@
+/*
+ * debug.h
+ *
+ *  Created on: Jun 28, 2018
+ *      Author: Poornachander
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+extern void debug_write(char *message);
+
+#endif /* DEBUG_H_ */
