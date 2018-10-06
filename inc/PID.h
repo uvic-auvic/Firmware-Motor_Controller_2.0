fde79dca503f5ceb3dbeed4531f81ceadf81ec96
Jayden Chan <jaydencn7@gmail.com>
2018-10-05 17:28:09 -0700

    Switch everything to #defines

--- inc/PID.h
+++ inc/PID.h
@@ -2,30 +2,15 @@
 #define PID_H_
 
 typedef struct PID_controller {
-    uint16_t P, I, D;
     uint32_t error;
     uint32_t error_sum, last_error;
     uint32_t output, last_output;
     uint32_t last_time;
-    uint16_t integral_limit, epsilon;
     uint16_t dt;
 } PID_controller_t;
 
-/* TODO: Add delay for overall PID update loop task */
-
-/**
- * Initializes the PID controller with the
- * provided values. There are many
- *
- * @param pid            The PID controller to initialize
- * @param kP             The P value for the controller
- * @param kI             The I value for the controller
- * @param kD             The D value for the controller
- * @param integral_limit The integral limit
- * @param epsilon        The epsilon value
- */
-static void PID_init(PID_controller_t *pid, uint32_t kP, uint32_t kI,
-        uint32_t kD, uint16_t integral_limit, uint16_t epsilon);
+/* TODO: Don't forget to add delay for overall PID
+ * update loop task */
 
 /**
  * Resets all critical values for the PID
@@ -33,7 +18,7 @@ static void PID_init(PID_controller_t *pid, uint32_t kP, uint32_t kI,
  *
  * @param pid The controller to reset
  */
-static void PID_reset(PID_controller_t *pid);
+void PID_reset(PID_controller_t *pid);
 
 /**
  * Computes the overall PID output using the
@@ -41,8 +26,8 @@ static void PID_reset(PID_controller_t *pid);
  *
  * @param pid   The PID controller to use
  * @param error Error to use for the calculation
- * @return The output value of the PID controller
+ * @return The output value of the PID controller (-100 - 100 %)
  */
-static uint32_t PID_calculate(PID_controller_t *pid, uint32_t error);
+int8_t PID_calculate(PID_controller_t *pid, uint32_t error);
 
 #endif
--- src/PID.c
+++ src/PID.c
@@ -1,32 +1,65 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "PID.h"
 
-#define BIT_K(N, K) (N >> K) & 1
+#define MOTOR_1_P 0
+#define MOTOR_2_P 0
+#define MOTOR_3_P 0
+#define MOTOR_4_P 0
+#define MOTOR_5_P 0
+#define MOTOR_6_P 0
+#define MOTOR_7_P 0
+#define MOTOR_8_P 0
 
-uint8_t zero_on_cross = 0b00000000;
-uint16_t delay = 20;
+#define MOTOR_1_I 0
+#define MOTOR_2_I 0
+#define MOTOR_3_I 0
+#define MOTOR_4_I 0
+#define MOTOR_5_I 0
+#define MOTOR_6_I 0
+#define MOTOR_7_I 0
+#define MOTOR_8_I 0
 
-static void PID_init(PID_controller_t *pid, uint32_t kP, uint32_t kI,
-                uint32_t kD, uint16_t integral_limit, uint16_t epsilon)
-{
-        pid->P = kP;
-        pid->I = kI;
-        pid->D = kD;
+#define MOTOR_1_D 0
+#define MOTOR_2_D 0
+#define MOTOR_3_D 0
+#define MOTOR_4_D 0
+#define MOTOR_5_D 0
+#define MOTOR_6_D 0
+#define MOTOR_7_D 0
+#define MOTOR_8_D 0
 
-        pid->integral_limit = integral_limit;
-        pid->epsilon = epsilon;
-}
+#define MOTOR_1_INT_LIM 0
+#define MOTOR_2_INT_LIM 0
+#define MOTOR_3_INT_LIM 0
+#define MOTOR_4_INT_LIM 0
+#define MOTOR_5_INT_LIM 0
+#define MOTOR_6_INT_LIM 0
+#define MOTOR_7_INT_LIM 0
+#define MOTOR_8_INT_LIM 0
+
+#define MOTOR_1_EPSILON 0
+#define MOTOR_2_EPSILON 0
+#define MOTOR_3_EPSILON 0
+#define MOTOR_4_EPSILON 0
+#define MOTOR_5_EPSILON 0
+#define MOTOR_6_EPSILON 0
+#define MOTOR_7_EPSILON 0
+#define MOTOR_8_EPSILON 0
+
+#define ZERO_ON_CROSS 0b00000000
+#define PID_DELAY 20
 
 static void PID_reset(PID_controller_t *pid)
 {
         pid->error       = 0;
-        pid->last_time   = 0;
-        pid->dt          = 10; /* dt is set to 10 to avoid divide by 0 error */
         pid->error_sum   = 0;
         pid->last_error  = 0;
         pid->output      = 0;
         pid->last_output = 0;
+        pid->last_time   = 0;
+        pid->dt          = PID_DELAY; /* Avoid div by 0 */
 }
 
 static uint32_t PID_calculate(PID_controller_t *pid, uint8_t motor, uint32_t error)
@@ -35,12 +68,12 @@ static uint32_t PID_calculate(PID_controller_t *pid, uint8_t motor, uint32_t err
         pid->dt = nPgmTime - pid->lastTime;
         pid->lastTime = nPgmTime;
 
-        pid->lastError = pid->error;
+        pid->last_error = pid->error;
         pid->error = error;
 
         float d_error = pid->dt != 0 ? (pid->error - pid->last_error) / pid->dt : 0;
 
-        if (pid->zeroOnCross && (sign(pid->error) != sign(pid->lastError))) {
+        if (pid->zeroOnCross && (sign(pid->error) != sign(pid->last_error))) {
                 pid->error_sum = 0;
         }
 
